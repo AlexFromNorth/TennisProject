bdc6066f275dc3c6b050162272f3408b85c2e9f7
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-08 01:52:51 +0300

    added move of ball to left/right

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ const ball = document.querySelector('.ball')
 console.log(wrapper.clientHeight)
 
 let up = 0
-let upVector = 1
+let upVector = 3
+
+let left = 0
+let leftVector = 3
 
 setInterval(()=>{
+    // up
     if(up > wrapper.clientHeight - ball.clientHeight || up < 0){
         upVector = -upVector
     }
     up += upVector
     ball.style.top = up + 'px'
+
+    // left
+    if(left > wrapper.clientWidth - ball.clientWidth || left < 0){
+        leftVector = -leftVector
+    }
+    left += leftVector
+    ball.style.left = left + 'px'
 
 }, 17)
